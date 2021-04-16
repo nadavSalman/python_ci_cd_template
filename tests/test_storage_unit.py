a38c215330f3f10d5599776b2589f4dfadc696c5
Nadav Salman <nadavops@gmail.com>
2021-04-16 14:37:13 +0300

    fix fail tests

--- tests/test_storage_unit.py
+++ tests/test_storage_unit.py
@@ -9,7 +9,7 @@ from storage.storage_units import false_function
 class TestStorageUnit(unittest.TestCase):
 
     def test_storage_item(self):
-        self.assertEqual(True, false_function())
+        self.assertEqual(False, false_function())
 
 
 
